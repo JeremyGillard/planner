2660b037fbac07a5eab5dd5bcc4cc3341684f11d
JeremyGillard <gillard.jeremy@outlook.fr>
2021-02-22 20:20:03 +0100

    cleanup

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd7b95cf9678b461c77f8d5e702c9420c'),
-    },
-  },
-});
--- app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '9387d19a-5b9e-4cfb-a5ab-d6051510ce5b'
-};
