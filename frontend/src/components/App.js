65388dc8cd01c67cd12bdcab67fabfc858326ddf
JeremyGillard <gillard.jeremy@outlook.fr>
2021-02-28 19:41:24 +0100

    feat(frontend): component cleaning

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,9 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import Day from './Day';
 
 export default function App() {
-  const [value, onChange] = useState(new Date());
-
   return (
     <div className="app">
-      <div>
-        <Calendar onChange={onChange} value={value} />
-      </div>
-      <div>
-        <Day pickedDate={value} />
-      </div>
+      <div>Application</div>
     </div>
   );
 }
